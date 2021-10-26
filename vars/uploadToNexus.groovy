e97d20695e90105a2012fd64ab1444fd25d5bab5
reddy9030 <yellareddyr999@gmail.com>
2021-10-27 00:07:46 +0530

    Create uploadToNexus.groovy

---
+++ vars/uploadToNexus.groovy
@@ -0,0 +1,14 @@
+deff call(nexuRepo){
+  def pom = readMavenPom file: 'pom.xml'
+	def repo = nexuRepo //pom.version.endsWith("SNAPSHOT") ? "my-app-snapshot" : "my-app-release"
+  def artifactId= pom.artifactId
+  def groupId= pom.groupId
+  nexusArtifactUploader artifacts: [[artifactId: 'myweb', classifier: '', file: "target/${artifactId}-${pom.version}.war", type: 'war']], 
+  credentialsId: 'Nexus3', 
+  groupId: 'in.javahome', 
+  nexusUrl: '172.31.9.153:8081', 
+  nexusVersion: 'nexus3', 
+  protocol: 'http', 
+  repository: repo, 
+  version: pom.version
+}
