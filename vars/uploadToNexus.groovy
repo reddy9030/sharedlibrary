77c616f33ab33f7fe10354f9b0f9b72eed264be8
reddy9030 <yellareddyr999@gmail.com>
2021-10-27 00:23:23 +0530

    Update uploadToNexus.groovy

--- vars/uploadToNexus.groovy
+++ vars/uploadToNexus.groovy
@@ -1,6 +1,6 @@
 deff call(nexusRepo){
   def pom = readMavenPom file: 'pom.xml'
-	def repo = nexusRepo //pom.version.endsWith("SNAPSHOT") ? "my-app-snapshot" : "my-app-release"
+  def repo = nexusRepo
   def artifactId= pom.artifactId
   def groupId= pom.groupId
   nexusArtifactUploader artifacts: [[artifactId: 'myweb', classifier: '', file: "target/${artifactId}-${pom.version}.war", type: 'war']], 
